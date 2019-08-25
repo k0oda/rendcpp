565b50a68eb2dd26f3a878663cbc3718b5800363
kukree <kulevgen32@gmail.com>
2019-08-26 01:53:50 +0600

    add: simple animation of moving "[]" structure up-down in main

--- main.cpp
+++ main.cpp
@@ -2,21 +2,40 @@
 
 using rend::Screen;
 
+#define BACKGROUND '-'
+
 int main(int argc, char const *argv[])
 {
     Screen screen(50, 100, 60, 120);
 
     for (int i = 0; i < 120; i++)
     {
-        screen.fill_frame(i);
+        screen.fill_frame(i, BACKGROUND);
+        screen.edit_frame(i, 50, 25, '[');
+        screen.edit_frame(i, 51, 25, ']');
     }
-    screen.render(120);
+    screen.render(5);
 
-    for (int i = 0; i < 120; i++)
+    while (true)
     {
-        screen.fill_frame(i, '*');
+        for (int i = 0; i < 120; i++)
+        {
+            screen.edit_frame(i, 50, 26, BACKGROUND);
+            screen.edit_frame(i, 51, 26, BACKGROUND);
+            screen.edit_frame(i, 50, 25, '[');
+            screen.edit_frame(i, 51, 25, ']');
+        }
+        screen.render(5);
+
+        for (int i = 0; i < 120; i++)
+        {
+            screen.edit_frame(i, 50, 25, BACKGROUND);
+            screen.edit_frame(i, 51, 25, BACKGROUND);
+            screen.edit_frame(i, 50, 26, '[');
+            screen.edit_frame(i, 51, 26, ']');
+        }
+        screen.render(5);
     }
-    screen.render(10);
 
     return 0;
 }
