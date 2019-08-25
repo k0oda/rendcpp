42139a7bd92d2badc4d5768607a979471755d3a4
kukree <kulevgen32@gmail.com>
2019-08-25 20:18:04 +0600

    add: simple animation in main

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@ using rend::Screen;
 
 int main(int argc, char const *argv[])
 {
-    Screen screen(15, 15, 60);
+    Screen screen(50, 100, 60, 120);
 
-    screen.init_scr();
+    for (int i = 0; i < 120; i++)
+    {
+        screen.fill_frame(i);
+    }
+    screen.render(120);
+
+    for (int i = 0; i < 120; i++)
+    {
+        screen.fill_frame(i, '*');
+    }
     screen.render(-1);
 
     return 0;
