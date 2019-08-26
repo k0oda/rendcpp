5bc28241c42e2423a380e3f70eed3f28a2900dc8
kukree <kulevgen32@gmail.com>
2019-08-26 22:58:20 +0600

    add: moving player system in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "rend.h"
-#include "sys/ioctl.h"
 
 using rend::Screen;
 
@@ -7,18 +6,18 @@ using rend::Screen;
 
 int main(int argc, char const *argv[])
 {
-    winsize w;
+    Screen screen(60, 120);
 
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    unsigned short x_center = screen.get_x_center();
+    unsigned short y_center = screen.get_y_center();
 
-    // Optimizing screen size
-    w.ws_col--;
-    w.ws_row--;
+    char ch;            // User input symbol
+    int y_offset = 0;
+    int x_offset = 0;
 
-    unsigned short x_center = w.ws_col / 2;
-    unsigned short y_center = w.ws_row / 2;
-
-    Screen screen(w.ws_row, w.ws_col, 60, 120);
+    // Centering player
+    int player_x = x_center;
+    int player_y = y_center;
 
     for (int i = 0; i < 120; i++)
     {
@@ -30,25 +29,47 @@ int main(int argc, char const *argv[])
 
     while (true)
     {
+        y_offset = 0;
+        x_offset = 0;
+
+        system("stty raw");
+        ch = getchar();
+        if (ch != 0)
+        {
+            switch(ch)
+            {
+                case 'w':
+                    y_offset = -1;
+                    break;
+                case 's':
+                    y_offset = 1;
+                    break;
+                case 'a':
+                    x_offset = -1;
+                    break;
+                case 'd':
+                    x_offset = 1;
+                    break;
+                case 'q':
+                    system("clear");
+                    return 0;
+            }
+        }
+        system("stty cooked");
+
+        player_x += x_offset;
+        player_y += y_offset;
+
         for (int i = 0; i < 120; i++)
         {
-            screen.edit_frame(i, x_center, y_center + 1, BACKGROUND);
-            screen.edit_frame(i, x_center + 1, y_center + 1, BACKGROUND);
+            screen.edit_frame(i, player_x - x_offset, player_y - y_offset, BACKGROUND);
+            screen.edit_frame(i, player_x - x_offset + 1, player_y - y_offset, BACKGROUND);
 
-            screen.edit_frame(i, x_center, y_center, '[');
-            screen.edit_frame(i, x_center + 1, y_center, ']');
+            screen.edit_frame(i, player_x, player_y, '[');
+            screen.edit_frame(i, player_x + 1, player_y, ']');
         }
-        screen.render(5);
-
-        for (int i = 0; i < 120; i++)
-        {
-            screen.edit_frame(i, x_center, y_center, BACKGROUND);
-            screen.edit_frame(i, x_center + 1, y_center, BACKGROUND);
-
-            screen.edit_frame(i, x_center, y_center + 1, '[');
-            screen.edit_frame(i, x_center + 1, y_center + 1, ']');
-        }
-        screen.render(5);
+        screen.render(1);
+        ch = 0;
     }
 
     return 0;
