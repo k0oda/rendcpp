708f5ee0130fa9a5ae502d1f02585f4da4052584
kukree <kulevgen32@gmail.com>
2019-08-26 22:53:13 +0600

    add: x_center and y_center variables

--- rend.h
+++ rend.h
@@ -15,6 +15,10 @@ namespace rend
         int height, width, FPS;
         float frame_time;
 
+        // Size and position variables
+        winsize terminal_size;
+        unsigned short x_center = terminal_size.ws_col / 2;
+        unsigned short y_center = terminal_size.ws_row / 2;
         std::vector<std::vector<char>> frame;
         std::vector<std::vector<std::vector<char>>> frame_list;
 
@@ -22,6 +26,8 @@ namespace rend
 
     public:
         Screen(unsigned int scr_height, unsigned int scr_width, unsigned int scr_FPS, unsigned int frames_count);
+        unsigned short get_x_center();
+        unsigned short get_y_center();
         void fill_frame(unsigned int frame_index, char fill_sym='#');
         void edit_frame(unsigned int frame_index, unsigned int x, unsigned int y, char sym=' ');
         void render(int repeat=1);
